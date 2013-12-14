e7207bdcd7f3d69ce0435d0429294daf47012e57
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-14 15:19:00 +0000

    file stub

---
+++ src/HSParedit.hs
@@ -0,0 +1,11 @@
+-- |
+-- Module      :  HSParedit
+-- Copyright   :  (c) Mateusz Kowalczyk 2013
+-- License     :  GPL-3
+--
+-- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
+-- Stability   :  experimental
+-- Portability :  portable
+
+module HSParedit where
+
