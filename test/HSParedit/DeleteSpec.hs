26d404b7b679335bf52a8fee37d1f6045e8a9ac9
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-15 01:38:20 +0000

    Spec failure pprint

--- test/HSParedit/DeleteSpec.hs
+++ test/HSParedit/DeleteSpec.hs
@@ -1,35 +1,60 @@
-{-# LANGUAGE CPP #-}
-
 module HSParedit.DeleteSpec (main, spec) where
 
 import HSParedit
 import Data.Tree
 import Data.Tree.Zipper
-import Test.Hspec
+import Data.Tree.Pretty
+import Test.Hspec (hspec, describe, it, Spec)
+import Test.Hspec.Expectations.Pretty
+import System.Console.Terminfo.PrettyPrint
+import Text.PrettyPrint.Free
+
+instance Show a => Pretty (Tree a) where
+  pretty = text . ("\n" ++) . drawVerticalTree . fmap show
+  prettyList = list . map pretty
+
+instance Show a => PrettyTerm (Tree a)
 
 main :: IO ()
 main = hspec spec
 
-basicTree' :: Tree String
-basicTree' = Node { rootLabel = "program"
-                 , subForest = [ Node { rootLabel = "foo"
-                                      , subForest = [] }
-                               , Node { rootLabel = "bar"
-                                      , subForest = [] }
-                               , Node { rootLabel = "baz"
-                                      , subForest = [ Node { rootLabel = "qux"
-                                                           ,  subForest = [] }
-                                                    ]
-                                      }
-                               ]
-                 }
+basicTree' :: Tree Code
+basicTree' = Node { rootLabel = TopLevel
+                  , subForest = [ sc "foo"
+                                , sc "bar"
+                                , Node { rootLabel = RoundNode
+                                       , subForest = [ sc "qux"
+                                                     , sc "quux"
+                                                     ]
+                                       }
+                                ]
+                  }
 
-zipTree' :: TreePos Full String
-zipTree' = root $ fromTree basicTree
+tinyTree :: Tree Code
+tinyTree = Node { rootLabel = TopLevel
+                , subForest = [sc "foo"]
+                }
+
+zipTree' :: TreePos Full Code
+zipTree' = fromTree basicTree
 
 spec :: Spec
 spec = do
-  describe "when deleting forward" $ do
-    it "ensures test sanity" $ do
-      1 == 1
+  describe "when checking sanity" $ do
+    it "ensures fromTree . toTree identity" $ do
+      fromTree (toTree zipTree') == zipTree'
 
+  describe "paren insertion" $ do
+    it "inserts parens at after code symbol if immediately after it" $ do
+      let tinyTree :: Tree Code
+          tinyTree = Node { rootLabel = TopLevel
+                          , subForest = [sc "foo"]
+                          }
+
+          editedTree :: Tree Code
+          editedTree = Node { rootLabel = TopLevel
+                            , subForest = [ sc "foo"
+                                          , Node RoundNode []
+                                          ]
+                            }
+        in tinyTree `shouldBe` editedTree
