fc5cbef5965c92809027febaa4376994cdc4e741
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2013-12-18 16:24:53 +0000

    Basic, flat printer

--- src/HSParedit/Printer.hs
+++ src/HSParedit/Printer.hs
@@ -11,8 +11,18 @@
 -- printing the AST into a LISP/Scheme format.
 module HSParedit.Printer where
 
+import Data.List (intersperse)
 import Data.Tree
 import HSParedit.Types
 
 toScheme :: Tree Code -> String
-toScheme = const "unimplemented"
+toScheme (Node CodeSymbol x2) = concat $ map toScheme x2
+toScheme (Node RoundNode x2) = wrap "(" ")" . unwords $ map toScheme x2
+toScheme (Node SquareNode x2) = wrap "[" "]" . unwords $ map toScheme x2
+toScheme (Node StringNode x2) = wrap "\"" "\"" . unwords $ map toScheme x2
+toScheme (Node (SymbolChar x1) _) = [x1]
+toScheme (Node TopLevel x2) = concat . intersperse "\n\n" $ map toScheme x2
+
+
+wrap :: String -> String -> String -> String
+wrap o e c = o ++ c ++ e
